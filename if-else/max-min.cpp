bff1c496152ac66ddc55f217cbb0164ab6c981b3
pratyushpriyadarshanmallik <93968652+pratyushpriyadarshanmallik@users.noreply.github.com>
2022-06-04 17:00:20 +0530

    Create max-min.cpp

---
+++ if-else/max-min.cpp
@@ -0,0 +1,16 @@
+
+#include<iostream>
+using namespace std;
+int main(){
+  int a,b;
+  cout<<"Enter number1:";
+  cin>>a;
+  cout<<"Enter number2:";
+  cin>>b;
+  if(a>b){
+    cout<<"number1 is maximum"<<endl<<"number2 is minimum"<<endl;
+  }else{
+    cout("number2 is maximum"<<endl<<"number1 is minimum"<<endl;
+         }
+   return 0;
+}     
