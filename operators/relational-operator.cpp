5327ad8410b0fbaa6699301415cfa685fba78651
pratyushpriyadarshanmallik <93968652+pratyushpriyadarshanmallik@users.noreply.github.com>
2022-06-06 20:32:17 +0530

    Create relational-operator.cpp

---
+++ operators/relational-operator.cpp
@@ -0,0 +1,19 @@
+//Input a number n and tell whether it is equal to,less than or more than 10
+#include <iostream>
+using namespace std;
+ 
+int main()
+{
+    int n;
+    cout<<"Enter a number:";
+    cin>>n;
+
+    if(n>10){
+        cout<<"More than 10"<<endl;
+    }else if(n<10){
+        cout<<"Less than 10"<<endl;
+    }else{
+        cout<<"Equal to 10"<<endl;
+    }
+    return 0;
+}
