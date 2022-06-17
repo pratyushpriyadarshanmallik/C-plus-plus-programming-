a05ac8ed3aa2561f82b79f45a833a0d7678f6827
pratyushpriyadarshanmallik <93968652+pratyushpriyadarshanmallik@users.noreply.github.com>
2022-06-17 21:07:19 +0530

    Create max-till-i.cpp

---
+++ array/max-till-i.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<climits>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter Size of array: ";
+    cin>>n;
+
+    int arr[n];
+    cout<<"Enter elements of array: ";
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+
+    int max=INT_MIN;
+    for(int i=0;i<n;i++){
+        if(arr[i]>max){
+            max=arr[i];
+            cout<<max<<" ";
+        }
+    }
+    return 0;
+}
