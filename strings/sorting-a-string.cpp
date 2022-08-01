a1a20382b7a50d333079b75a81234b5db4750678
pratyushpriyadarshanmallik <93968652+pratyushpriyadarshanmallik@users.noreply.github.com>
2022-08-01 22:59:25 +0530

    Create sorting-a-string.cpp

---
+++ strings/sorting-a-string.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<algorithm>
+#include<string>
+using namespace std;
+int main(){  
+    string s1="ksahakgutauiosduyy";
+
+    sort(s1.begin(),s1.end());
+    cout<<s1<<endl;
+    return 0;
+}
