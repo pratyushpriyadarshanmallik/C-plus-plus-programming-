6a742dc5da75a27d0b1e3915c88ceaff2d25947e
pratyushpriyadarshanmallik <93968652+pratyushpriyadarshanmallik@users.noreply.github.com>
2022-10-27 14:47:11 +0000

    Update advanced-recursion-problems.cpp

--- recursion/advanced-recursion-problems.cpp
+++ recursion/advanced-recursion-problems.cpp
@@ -1,10 +1,17 @@
-//Print all possible permutations of a string 
-$include<iostream>
+#include<iostream>
 using namespace std;
-void permutations (string s, string ans){
- if(s.length()==0){
- cout<<ans<<endl;
- return;
-  }
-for(int i=0;i<s.length ();i++){
-  char ch=s[i];
+//Print all possible permutations of a string 
+void permutations(string s,string ans){
+    if(s.length()==0){
+        cout<<ans<<endl;
+        return;
+    }
+    for(int i=0;i<s.length();i++){
+        char ch=s[i];
+        string ros =s.substr(0,i)/*element before ith element*/+s.substr(i+1)/*element after ith element */;
+        permutations(ros,ans+ch);
+    }
+}
+int main(){
+    permutations("ABC","");
+}
