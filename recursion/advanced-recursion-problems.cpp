2436a9eaf73ba55169e9bee7547a9b6ec58b3fa9
pratyushpriyadarshanmallik <93968652+pratyushpriyadarshanmallik@users.noreply.github.com>
2022-10-27 15:10:43 +0000

    Update advanced-recursion-problems.cpp

--- recursion/advanced-recursion-problems.cpp
+++ recursion/advanced-recursion-problems.cpp
@@ -26,8 +26,20 @@ int countpath(int s,int e){
     }
     return count;
 }
+//Count the number of paths possible in a maze
+int countPathMaze(int n,int i,int j){
+    if(i==n-1 && j==n-1){
+        return 1;
+    }
+    if(i>n || j>n){
+        return 0;
+    }
+    return countPathMaze(n,i+1,j) + countPathMaze(n,i,j+1);
+}
 int main(){
     permutations("ABC","");
     cout<<endl;
     cout<<countpath(0,3);
+    cout<<endl;
+    cout<<countPathMaze(3,0,0);
 }
