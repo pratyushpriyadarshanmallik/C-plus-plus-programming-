8141551aaabd5461c1ca9c76c85f9062e3870519
pratyushpriyadarshanmallik <93968652+pratyushpriyadarshanmallik@users.noreply.github.com>
2022-10-27 15:45:09 +0000

    Update advanced-recursion-problems.cpp

--- recursion/advanced-recursion-problems.cpp
+++ recursion/advanced-recursion-problems.cpp
@@ -53,6 +53,16 @@ int friendspairing(int n){
     }
     return friendspairing(n-1) + friendspairing(n-2)*(n-1);
 }
+//0-1 knapsack problem
+int knapsack(int value[],int wt[],int n,int W){
+    if(n==0 || W==0){
+        return 0;
+    }
+    if(wt[n-1]>W){
+        return knapsack(value,wt,n-1,W);/*When weight of the nth element is more than the weight limit we will not include it*/
+    }
+    return max(knapsack(value,wt,n-1,W-wt[n-1])+value[n-1]/*When we include nth item */,knapsack(value,wt,n-1,W)/*When we dont include nth item*/);
+}
 int main(){
     permutations("ABC","");
     cout<<endl;
@@ -63,4 +73,9 @@ int main(){
     cout<<tilingways(4);
     cout<<endl;
     cout<<friendspairing(4);
+    cout<<endl;
+    int wt[]={10,20,30};
+    int value[]={100,50,150};
+    int W=50;
+    cout<<knapsack(value,wt,3,W);
 }  
