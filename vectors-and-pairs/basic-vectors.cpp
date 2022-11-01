ce3849184fb35d7fba4160de4c48655d8e782c9b
pratyushpriyadarshanmallik <93968652+pratyushpriyadarshanmallik@users.noreply.github.com>
2022-11-01 14:58:53 +0000

    Update and rename vectors-and -pairs/basic-vectors.cpp to vectors-and-pairs/basic-vectors.cpp

--- vectors-and-pairs/basic-vectors.cpp
+++ vectors-and-pairs/basic-vectors.cpp
@@ -3,9 +3,9 @@
 using namespace std;
 int main(){
     vector <int> v;
-    v.push_back(1);
-    v.push_back(2);
     v.push_back(3);
+    v.push_back(2);
+    v.push_back(1);
     //Printing vector using loops
     for(int i=0;i<v.size();i++){
         cout<<v[i]<<" ";//123
@@ -45,5 +45,26 @@ int main(){
     for(itt=v2.begin();itt!=v2.end();itt++){
         cout<<*itt<<" ";
     }
+    cout<<endl;
+
+    //Swap function
+    swap(v,v2);
+    for(auto element :v){
+        cout<<element<<" ";
+    }
+    cout<<endl;
+
+    for(auto element :v2){
+        cout<<element<<" ";
+    }
+    cout<<endl;
+    
+    //Sorting function
+    //sort(v.begin(),v.end());
+    for(auto element :v){
+        cout<<element<<" ";
+    }
+    cout<<endl;
+    
     return 0;
 }
